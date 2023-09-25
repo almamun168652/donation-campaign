2f8cbc07fdd9590d06397b2e756b1fa50dd5ffc3
Al Mamun <wdmamun75@gmail.com>
2023-09-25 10:22:28 +0600

    complete donated item show inside the display and see all button condition

--- src/Components/AllDonation.jsx
+++ src/Components/AllDonation.jsx
@@ -1,11 +1,34 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
+const AllDonation = ({ donateItem }) => {
 
-const AllDonation = () => {
+    const { id, style, title, category, thumbnail, price } = donateItem || {}
+
     return (
         <div>
-            all donations
+            <div style={{ backgroundColor: style.card_bg }} className='flex'>
+                <div>
+                    <img className='w-48' src={thumbnail} alt="" />
+                </div>
+                <div className='p-5'>
+                    <div className=''>
+                        <span style={{ backgroundColor: style.btn_bg, color: style.t_b_color }} className='py-1 px-2 font-semibold rounded text-sm'>{category}</span>
+                        <h1 className='font-bold text-xl mt-1 text-[#0B0B0B]'>{title}</h1>
+                        <p style={{ color: style.t_b_color }} className='font-bold my-2'>${price}</p>
+                        <Link to={`/details/${id}`}>
+                            <button className='py-1 rounded px-3 font-semibold text-white mt-1' style={{ backgroundColor: style?.t_b_color }}>View Details</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+AllDonation.propTypes = {
+    donateItem: PropTypes.object.isRequired
+}
+
 
 export default AllDonation;
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -5,29 +5,37 @@ import AllDonation from "../../Components/AllDonation";
 
 const Donation = () => {
 
-    const [donates , setDonates] = useState([]);
-    const [noFound , setNoFound] = useState('');
+    const [donates, setDonates] = useState([]);
+    const [noFound, setNoFound] = useState('');
+    const [isShow, setIsShow] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
 
         const donationsItem = JSON.parse(localStorage.getItem('donations'));
 
-        if(donationsItem){
+        if (donationsItem) {
             setDonates(donationsItem);
-        }else{
+        } else {
             setNoFound(['No Data Found']);
         }
 
-    },[])
+    }, [])
 
     return (
-        <div>
+        <div className="max-w-screen-xl mx-auto px-4 md:px-8 mt-20">
             {
                 noFound ? <p className="mt-36 text-2xl font-bold flex items-center justify-center">{noFound}</p> : <div>
                     <div>
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                             {
-                                donates.map(donateItem => <AllDonation key={donateItem.id} donateItem={donateItem}></AllDonation>)
+                                isShow ? donates.map(donateItem => <AllDonation key={donateItem.id} donateItem={donateItem}></AllDonation>)
+                                    :
+                                    donates.slice(0, 4).map(donateItem => <AllDonation key={donateItem.id} donateItem={donateItem}></AllDonation>)
+                            }
+                        </div>
+                        <div className="text-center my-10">
+                            {
+                                donates.length > 4 && <button className={isShow ? 'hidden' : 'visible bg-[#009444] text-white font-semibold rounded-lg px-5 py-2'} onClick={() => setIsShow(true)}>See All</button>
                             }
                         </div>
                     </div>
